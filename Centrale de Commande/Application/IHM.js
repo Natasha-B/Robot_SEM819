3362221197bd3d309c16b1cd44061dc192d32bd4
Nataha-B <natachabru@mbp-de-natacha.home>
2019-02-27 11:30:05 +0100

    Port série

--- Centrale de Commande/Application/IHM.js
+++ Centrale de Commande/Application/IHM.js
@@ -11,11 +11,43 @@
 // Groupe: B2
 //
 //------------------------------------------------------------------------------------
-
 var express = require ('express');
 var app = express();
 var http = require('http');
 var server = http.Server(app);
+
+//------------------------------------------------------------------------------------
+
+// Port série https://serialport.io
+const SerialPort = require('serialport')
+const Readline = require('@serialport/parser-readline')
+const port = new SerialPort('/dev/tty-usbserial1', { baudRate: 256000 })
+
+const parser = new Readline()
+port.pipe(parser)
+
+parser.on('data', line => console.log(`> ${line}`))
+
+// Envoi du message de début
+port.write('ROBOT POWER ON\n', function(err) {
+  if (err) {
+    return console.log('Error on write: ', err.message)
+  }
+  console.log('message written')
+})
+
+// Gestion des erreurs
+port.on('error', function(err) {
+  console.log('Error: ', err.message)
+})
+
+
+// Switches the port into "flowing mode"
+port.on('data', function (data) {
+  console.log('Data:', data)
+})
+
+//------------------------------------------------------------------------------------
 
 app.use(express.static(__dirname + '/public'));
 
@@ -30,5 +62,7 @@ app.get('/assistance', function(req,res){
 app.get('/assistance/:nom', function(req,res){
 	res.render('assistance.ejs', {"nom": req.params.nom});
 });
+
+//------------------------------------------------------------------------------------
 
 server.listen(8080);
