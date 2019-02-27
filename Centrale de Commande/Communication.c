ff5d22ad442935c2619dfe8c15ebd12310c58641
Nataha-B <natachabru@mbp-de-natacha.home>
2019-02-27 09:04:08 +0100

    Communication série

--- Centrale de Commande/Communication.c
+++ Centrale de Commande/Communication.c
@@ -1,20 +1,19 @@
 //------------------------------------------------------------------------------------
-// Projet_BSE.c
+// Projet_Transversal
 //------------------------------------------------------------------------------------
-// DATE: 19/12/18
+// DATE: 27/02/19
 //
 // Target: C8051F02x
 // Tool chain: KEIL Microvision 4
 //
-//  NOMS: Albert, Avinain, Bru, Jannin
+//  NOMS: Blasco, Bru, Renotton
 //
-// Numéro du sujet: 1
-// Binôme : B (Avinain, Bru)
+// Groupe: B2
 //
 //------------------------------------------------------------------------------------
 #include "c8051F020.h"
-#include "Binome_A.h"
-#include "Binome_B.h"
+#include "Communication.h"
+
 
 // UART
 char UART_buff;
@@ -24,10 +23,6 @@ char UART_busy = 0;
 sfr16 T2 = 0xcc;
 sfr16 RCAP2 = 0xCA;
 
-// CAN
-sfr16 ADC0 = 0xBE;
-float CSG_min = 0.0f;
-float CSG_max = 30.0f;
 
 //------------------------------------------------------------------------------------
 // CONFIGURATION
@@ -110,22 +105,6 @@ void cfg_interrupt (void) {
 	EA = 1; // enable all interupts
 }
 
-/**
- * Configuration du convertisseur analogique/numérique
- * @param void
- * @return void
- * Registres modifiés : ADC0CN, AMX0CF, AMX0SL, REF0CN
- */
-void cfg_CAN (void) {
-	REF0CN = 0x03;
-	AMX0CF = 0x0;
-	AMX0SL = 0x0; // AIN0 par défaut
-	AD0CM0 = 0; //conversion pour AD0BUSY = 1
-	AD0CM1 = 0; 
-	AD0TM = 0;
-	AD0LJST = 0; //alignement à droite
-	AD0EN = 1; // activation du  CAN
-}
 
 
 //------------------------------------------------------------------------------------
@@ -142,18 +121,10 @@ void init (void) {
 	cfg_clk();
 	cfg_UART();
 	cfg_timer2();
-	cfg_CAN();
 	cfg_interrupt();
 	
 	XBR2 |= 1<<6;	//Activation du Crossbar
 	
-	cfg_LED();
-	cfg_timer3();
-	cfg_timer4();
-	cfg_servo();
-	
-	EIE2 = 0x05;  // Activation des interruptions Timers 3 et 4
-	EIP2 = 0x03;  // Priorité haute Timers 3 et 4
 }
 
 /**
@@ -239,7 +210,7 @@ void UART_sendi (char *prefix, int a, char *suffix) {
 
 
 /**
- * Réception d'un caractère sur l'UART (fait appel à des fonctions développées par le binome A)
+ * Réception d'un caractère sur l'UART 
  * @param void
  * @return void
  */
@@ -315,83 +286,6 @@ void UART() interrupt 4 {
 }
 
 
-/**
- * Lecture de consigne distance min
- * @param void
- * @return int CSG min
- * Registres modifiés : AMX0SL, AD0INT, AD0BUSY
- */
-int get_CSG_min(void) {
-	AMX0SL = 0x01; // AIN1
-	AD0INT = 0;
-	AD0BUSY = 1;
-	while(AD0INT != 1);
-	
-	CSG_min = (50.0f*((ADC0*2.75f)/4095.0f));
-	return CSG_min;
-}
-
-/**
- * Lecture de consigne distance max
- * @param void
- * @return int CSG max
- * Registres modifiés : AMX0SL, AD0INT, AD0BUSY
- */
-int get_CSG_max(void) {
-	AMX0SL = 0x02; // AIN2
-	AD0INT = 0;
-	AD0BUSY = 1;
-	while(AD0INT != 1);
-	
-	CSG_max = (50.0f*((ADC0*2.75f)/4095.0f));
-	return CSG_max;
-}
-
-/**
- * Activation du mode "Balayage automatique"
- * @param void
- * @return void
- * Registres modifiés : 
- */
-void auto_mode() {
-	float angle = -90.0f;
-	
-	UART_sends("AUTOMATIC MODE ACTIVATED!");
-	UART_sendCRLF();
-	
-	while(angle <= 90.0f) {
-		run(angle);
-		angle += 10.0f;
-	}
-}
-
-/**
- * Affichage d'un diagramme de positionnement
- * @param void
- * @return void
- * Registres modifiés : 
- */
-void show_diagram(float d) {
-	int pos;
-	char diag[22] = " |                  | ";
-	
-	// Trop près
-	if(d < CSG_min) {
-		diag[0] = 'X';
-		
-	// Trop loin
-	} else if(d > CSG_max) {
-		diag[21] = 'X';
-		
-	// Dans l'intervalle
-	} else {
-		pos = (int) (20.0f * (d - CSG_min) / (CSG_max - CSG_min));
-		diag[pos+1] = 'v';
-	}
-	
-	UART_sends(diag);
-	UART_sendCRLF();
-}
 
 //------------------------------------------------------------------------------------
 // APPLICATION
@@ -405,61 +299,10 @@ void show_diagram(float d) {
 void Welcome() {
 	UART_sendCRLF();
 	UART_sendCRLF();
-	UART_sends("********** CONTROLE DE DISTANCE **********");
+	UART_sends("********** CENTRALE DE COMMANDE **********");
 	UART_sendCRLF();
 	UART_sends("Bienvenue :)");
 	UART_sendCRLF();
 }
 
-/**
- * Effectue une mesure à l'angle de rotation actuel
- * @param void
- * @return void
- */
-float mesure (int angle) {
-	float V, d;
-	
-	AMX0SL = 0x0; // AIN0
-	AD0INT = 0;
-	AD0BUSY = 1;
-	while(AD0INT != 1);
-	
-	V = ((ADC0*2.75f)/4095.0f);
-	if(V < 2.0f)
-		d = (1.0f/(0.0169f*V-0.0008f));
-	else
-		d = (1.0f/(0.0382f*V-0.0454f));
-	
-	if(angle >= 0)
-		UART_sendi("Mesure (", angle, "deg): ");
-	else
-		UART_sendi("Mesure (-", -angle, "deg): ");
-	UART_sendi("", (int) d, "cm");
-	show_diagram(d);
-	return d;
-}
 
-/**
- * Effectue une mesure à un angle donné
- * @param int angle, angle de mesure (en degrès)
- * @return void
- */
-void run (int angle) {
-	float d;
-	
-	// Lecture des consignes
-	get_CSG_min();
-	get_CSG_max();
-	
-	// Rotation à l'angle souhaité
-	servo_pos(angle);
-	
-	// Réalisation de la mesure
-	d = mesure(angle);
-	
-	// Vérification
-	if((d >= CSG_min) && (d <= CSG_max))
-		start_blink_led();
-	else
-		stop_blink_led();
-}
